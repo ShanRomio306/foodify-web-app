ebe7e45b6bda3867e593df4ad6204ae80f4f4411
ShanRomio306 <23br15919@rit.ac.in>
2026-03-08 00:46:41 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_REACT_APP_BACKEND_URL}`, // your backend URL
+  baseURL: import.meta.env.VITE_REACT_APP_BACKEND_URL, // your backend URL
   withCredentials: false
 });
 
