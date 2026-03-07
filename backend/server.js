14a45d424b59b9cc05f8175f652f15af92ee20ad
ShanRomio306 <23br15919@rit.ac.in>
2026-03-08 01:15:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,56 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { connectDB } from './config/db.js';
-import userRoutes from "./routes/userRoutes.js";
-import restRoutes from "./routes/restRoutes.js"
-import paymentRoutes from "./routes/paymentRoutes.js"
-import offerRoutes from "./routes/offerRoutes.js"
-import menuRoutes from "./routes/menuRoutes.js"
-import cartRoutes from "./routes/cartRoutes.js"
-import authRoutes from "./routes/authRoutes.js"
-import restOrder from "./routes/orderRoutes/restOrder.js"
-import userOrder from "./routes/orderRoutes/userOrder.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
-const app = express();
+import userRoutes from "./routes/userRoutes.js";
+import restRoutes from "./routes/restRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import offerRoutes from "./routes/offerRoutes.js";
+import menuRoutes from "./routes/menuRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import restOrder from "./routes/orderRoutes/restOrder.js";
+import userOrder from "./routes/orderRoutes/userOrder.js";
+
 dotenv.config();
 
+const app = express();
 
-app.use(cors({
-  origin: import.meta.env.URL,
-  credentials: true
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://foodify-web-app-front.vercel.app",
+];
 
 app.use(express.json());
 
-// app.get("/api/menu", (req,res) => {
-//     res.send("1.Appam\n2.Chicken Curry\n3.Meals\n");
-// })
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error("CORS blocked for origin: " + origin));
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
+app.options("*", cors());
+
+// routes
 app.use("/", userRoutes);
 app.use("/", restRoutes);
 app.use("/", paymentRoutes);
 app.use("/", offerRoutes);
-//app.use("/", orderRoutes)
 app.use("/", menuRoutes);
 app.use("/", cartRoutes);
 app.use("/", authRoutes);
 app.use("/", restOrder);
 app.use("/", userOrder);
 
+// connect db once per cold start
+connectDB();
 
-connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-    console.log("Server Started on port 5001");
-    });
-});
-
-
-//console.log("Hello")
+export default app;
